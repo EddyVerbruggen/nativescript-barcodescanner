e25311733b811e5d4f7e1b52104eb425c7cd3938
Eddy Verbruggen <eddyverbruggen@gmail.com>
2015-08-15 20:35:19 +0200

    Added iOS implementation

--- barcodescanner.ios.js
+++ barcodescanner.ios.js
@@ -1,11 +1,78 @@
 var barcodescanner = require("./barcodescanner-common");
+var frame = require("ui/frame");
 
-barcodescanner.scan = function(arg) {
-  return new Promise(function(resolve, reject) {
-    // TODO implement
-    resolve(true);
+barcodescanner.available = function () {
+  return new Promise(function (resolve) {
+    resolve(QRCodeReader.isAvailable());
   });
 };
 
-// TODO doesn't common do that already?
+// TODO check camera PERMISSION beforehand: https://github.com/yannickl/QRCodeReaderViewController/issues/4
+barcodescanner.scan = function (arg) {
+  return new Promise(function (resolve, reject) {
+    try {
+      var closeButtonLabel = arg.cancelLabel || "Close";
+
+      var types = [AVMetadataObjectTypeUPCECode, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeCode39Mod43Code,
+        AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeCode128Code,
+        AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeQRCode, AVMetadataObjectTypeAztecCode];
+
+      var bs = QRCodeReaderViewController.readerWithCancelButtonTitleMetadataObjectTypes(closeButtonLabel, types);
+      bs.modalPresentationStyle = UIModalPresentationFormSheet;
+
+      // Assign first to local variable, otherwise it will be garbage collected since delegate is weak reference.
+      var delegate = QRCodeReaderDelegateImpl.new().initWithCallback(function (reader, text, type) {
+        // invoke the callback / promise
+        if (text === undefined) {
+          reject("Scan aborted");
+        } else {
+          resolve({
+            format : type,
+            text : text
+          });
+        }
+        // Remove the local variable for the delegate.
+        delegate = undefined;
+      });
+      bs.delegate = delegate;
+
+      var topMostFrame = frame.topmost();
+      if (topMostFrame) {
+        var vc = topMostFrame.currentPage && topMostFrame.currentPage.ios;
+        if (vc) {
+          vc.presentViewControllerAnimatedCompletion(bs, true, null);
+        }
+      }
+    } catch (ex) {
+      console.log("Error in barcodescanner.scan: " + ex);
+      reject(ex);
+    }
+  });
+};
+
+var QRCodeReaderDelegateImpl = (function (_super) {
+  __extends(QRCodeReaderDelegateImpl, _super);
+  function QRCodeReaderDelegateImpl() {
+    _super.apply(this, arguments);
+  }
+
+  QRCodeReaderDelegateImpl.new = function () {
+    return _super.new.call(this);
+  };
+  QRCodeReaderDelegateImpl.prototype.initWithCallback = function (callback) {
+    this._callback = callback;
+    return this;
+  };
+  QRCodeReaderDelegateImpl.prototype.readerDidCancel = function (reader) {
+    UIApplication.sharedApplication().keyWindow.rootViewController.dismissViewControllerAnimatedCompletion(true, null);
+    this._callback(reader);
+  };
+  QRCodeReaderDelegateImpl.prototype.readerDidScanResultForType = function (reader, text, type) {
+    UIApplication.sharedApplication().keyWindow.rootViewController.dismissViewControllerAnimatedCompletion(true, null);
+    this._callback(reader, text, type);
+  };
+  QRCodeReaderDelegateImpl.ObjCProtocols = [QRCodeReaderDelegate];
+  return QRCodeReaderDelegateImpl;
+})(NSObject);
+
 module.exports = barcodescanner;
---
+++ platforms/ios/BarcodeScannerFramework.framework/Headers/QRCodeReaderViewController.h
@@ -0,0 +1,161 @@
+#import <UIKit/UIKit.h>
+#import "QRCodeReaderDelegate.h"
+#import "QRCodeReader.h"
+
+/**
+ * Convenient controller to display a view to scan/read 1D or 2D bar codes like
+ * the QRCodes. It is based on the `AVFoundation` framework from Apple. It aims
+ * to replace ZXing or ZBar for iOS 7 and over.
+ */
+@interface QRCodeReaderViewController : UIViewController
+
+#pragma mark - Creating and Inializing QRCodeReader Controllers
+/** @name Creating and Inializing QRCode Reader Controllers */
+
+/**
+ * @abstract Initializes a view controller to read QRCodes from a displayed
+ * video preview and a cancel button to be go back.
+ * @param cancelTitle The title of the cancel button.
+ * @discussion This convenient method is used to instanciate a reader with
+ * only one supported metadata object types: the QRCode.
+ * @see initWithCancelButtonTitle:metadataObjectTypes:
+ * @since 1.0.0
+ */
+- (id)initWithCancelButtonTitle:(NSString *)cancelTitle;
+
+/**
+ * @abstract Creates a view controller to read QRCodes from a displayed
+ * video preview and a cancel button to be go back.
+ * @param cancelTitle The title of the cancel button.
+ * @see initWithCancelButtonTitle:
+ * @since 1.0.0
+ */
++ (instancetype)readerWithCancelButtonTitle:( NSString *)cancelTitle;
+
+/**
+ * @abstract Initializes a reader view controller with a list of metadata
+ * object types.
+ * @param metadataObjectTypes An array of strings identifying the types of
+ * metadata objects to process.
+ * @see initWithCancelButtonTitle:metadataObjectTypes:
+ * @since 3.0.0
+ */
+- ( id)initWithMetadataObjectTypes:( NSArray *)metadataObjectTypes;
+
+/**
+ * @abstract Creates a reader view controller with a list of metadata object
+ * types.
+ * @param metadataObjectTypes An array of strings identifying the types of
+ * metadata objects to process.
+ * @see initWithMetadataObjectTypes:
+ * @since 3.0.0
+ */
++ ( instancetype)readerWithMetadataObjectTypes:( NSArray *)metadataObjectTypes;
+
+/**
+ * @abstract Initializes a view controller to read wanted metadata object
+ * types from a displayed video preview and a cancel button to be go back.
+ * @param cancelTitle The title of the cancel button.
+ * @param metadataObjectTypes The type (“symbology”) of barcode to scan.
+ * @see initWithCancelButtonTitle:codeReader:
+ * @since 2.0.0
+ */
+- ( id)initWithCancelButtonTitle:( NSString *)cancelTitle metadataObjectTypes:( NSArray *)metadataObjectTypes;
+
+/**
+ * @abstract Creates a view controller to read wanted metadata object types
+ * from a displayed video preview and a cancel button to be go back.
+ * @param cancelTitle The title of the cancel button.
+ * @param metadataObjectTypes The type (“symbology”) of barcode to scan.
+ * @see initWithCancelButtonTitle:metadataObjectTypes:
+ * @since 2.0.0
+ */
++ ( instancetype)readerWithCancelButtonTitle:( NSString *)cancelTitle metadataObjectTypes:( NSArray *)metadataObjectTypes;
+
+/**
+ * @abstract Initializes a view controller using a cancel button title and
+ * a code reader.
+ * @param cancelTitle The title of the cancel button.
+ * @param codeReader The reader to decode the codes.
+ * @see initWithCancelButtonTitle:codeReader:startScanningAtLoad:
+ * @since 3.0.0
+ */
+- ( id)initWithCancelButtonTitle:( NSString *)cancelTitle codeReader:( QRCodeReader *)codeReader;
+
+/**
+ * @abstract Initializes a view controller using a cancel button title and
+ * a code reader.
+ * @param cancelTitle The title of the cancel button.
+ * @param codeReader The reader to decode the codes.
+ * @see initWithCancelButtonTitle:codeReader:
+ * @since 3.0.0
+ */
++ ( instancetype)readerWithCancelButtonTitle:( NSString *)cancelTitle codeReader:( QRCodeReader *)codeReader;
+
+/**
+ * @abstract Initializes a view controller using a cancel button title and
+ * a code reader.
+ * @param cancelTitle The title of the cancel button.
+ * @param codeReader The reader to decode the codes.
+ * @param startScanningAtLoad Flag to know whether the view controller start scanning the codes when the view will appear.
+ * @since 3.0.0
+ */
+- ( id)initWithCancelButtonTitle:( NSString *)cancelTitle codeReader:( QRCodeReader *)codeReader startScanningAtLoad:(BOOL)startScanningAtLoad;
+
+/**
+ * @abstract Initializes a view controller using a cancel button title and
+ * a code reader.
+ * @param cancelTitle The title of the cancel button.
+ * @param codeReader The reader to decode the codes.
+ * @param startScanningAtLoad Flag to know whether the view controller start scanning the codes when the view will appear.
+ * @see initWithCancelButtonTitle:codeReader:startScanningAtLoad:
+ * @since 3.0.0
+ */
++ ( instancetype)readerWithCancelButtonTitle:( NSString *)cancelTitle codeReader:( QRCodeReader *)codeReader startScanningAtLoad:(BOOL)startScanningAtLoad;
+
+#pragma mark - Controlling the Reader
+/** @name Controlling the Reader */
+
+/**
+ * @abstract Starts scanning the codes.
+ * @since 3.0.0
+ */
+- (void)startScanning;
+
+/**
+ * @abstract Stops scanning the codes.
+ * @since 3.0.0
+ */
+- (void)stopScanning;
+
+#pragma mark - Managing the Delegate
+/** @name Managing the Delegate */
+
+/**
+ * @abstract The object that acts as the delegate of the receiving QRCode
+ * reader.
+ * @since 1.0.0
+ */
+@property (nonatomic, strong) id<QRCodeReaderDelegate> delegate;
+
+/**
+ * @abstract Sets the completion with a block that executes when a QRCode
+ * or when the user did stopped the scan.
+ * @param completionBlock The block to be executed. This block has no
+ * return value and takes one argument: the `resultAsString`. If the user
+ * stop the scan and that there is no response the `resultAsString` argument
+ * is nil.
+ * @since 1.0.1
+ */
+- (void)setCompletionWithBlock:( void (^) (NSString * resultAsString, NSString * typeAsString))completionBlock;
+
+#pragma mark - Managing the Reader
+/** @name Managing the Reader */
+
+/**
+ * @abstract The default code reader created with the controller.
+ * @since 3.0.0
+ */
+@property (strong, nonatomic, readonly) QRCodeReader * codeReader;
+
+@end
