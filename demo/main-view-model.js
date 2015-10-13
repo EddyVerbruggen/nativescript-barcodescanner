deb2faea4a87b06667592d85e07d6d8b985a528a
Eddy Verbruggen <eddyverbruggen@gmail.com>
2015-10-13 09:56:04 +0200

    Added a very basic demo

---
+++ demo/main-view-model.js
@@ -0,0 +1,42 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var observable = require("data/observable");
+  var barcodescanner = require("nativescript-barcodescanner");
+var HelloWorldModel = (function (_super) {
+    __extends(HelloWorldModel, _super);
+    function HelloWorldModel() {
+        _super.call(this);
+        this.counter = 42;
+        this.set("message", this.counter + " taps left");
+    }
+    HelloWorldModel.prototype.tapAction = function () {
+        this.counter--;
+barcodescanner.scan({
+    cancelLabel: "Stop scanning", // iOS only, default 'Close' 
+    message: "Go scan something", // Android only, default is 'Place a barcode inside the viewfinder rectangle to scan it.' 
+    preferFrontCamera: false,     // Android only, default false 
+    showFlipCameraButton: true    // Android only, default false (on iOS it's always available) 
+  }).then(
+      function(result) {
+        console.log("Scan format: " + result.format);
+        console.log("Scan text:   " + result.text);
+      },
+      function(error) {
+        console.log("No scan: " + error);
+      }
+  );
+          if (this.counter <= 0) {
+            this.set("message", "Hoorraaay! You unlocked the NativeScript clicker achievement!");
+        }
+        else {
+            this.set("message", this.counter + " taps left");
+        }
+    };
+    return HelloWorldModel;
+})(observable.Observable);
+exports.HelloWorldModel = HelloWorldModel;
+exports.mainViewModel = new HelloWorldModel();
