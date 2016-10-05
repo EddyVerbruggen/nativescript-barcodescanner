ffc3bf7f7b0cbe18d6236c30449a7c52e93e038c
Joe Skeen <joe.skeen@imail.org>
2016-10-05 08:46:54 -0600

    Improve typings

--- barcodescanner.d.ts
+++ barcodescanner.d.ts
@@ -1,8 +1,6 @@
-declare module "nativescript-barcodescanner" {
-    /**
-     * The options object passed into the scan function.
-     */
-    export interface ScanOptions {
+declare namespace barcodeScanner {
+  namespace ScanOptions {
+    interface Common {
       /**
        * A comma sep. string of barcode types: "QR_CODE,PDF_417"
        * Default: empty, so all types of codes can be scanned.
@@ -10,12 +8,27 @@ declare module "nativescript-barcodescanner" {
       formats?: string;
 
       /**
+       * By default the scanned object is returned in the Promise,
+       * but if you want to scan continuously (until you call 'stop'),
+       * you can provide a callback function that receives the same object
+       * as the Promise would, but every time something is scanned.
+       *
+       * This function doesn't report duplicates in the same scanning session,
+       * you're welcome ;)
+       */
+      continuousScanCallback?: Function;
+    }
+
+    interface IOS extends Common {
+      /**
        * The label of the button used to close the scanner.
        * Default: "Close".
        * iOS only.
        */
       cancelLabel?: string;
+    }
 
+    interface Android extends Common {
       /**
        * The message shown when looking for something to scan.
        * Default: "Place a barcode inside the viewfinder rectangle to scan it."
@@ -31,17 +44,6 @@ declare module "nativescript-barcodescanner" {
       preferFrontCamera?: boolean;
 
       /**
-       * By default the scanned object is returned in the Promise,
-       * but if you want to scan continuously (until you call 'stop'),
-       * you can provide a callback function that receives the same object
-       * as the Promise would, but every time something is scanned.
-       * 
-       * This function doesn't report duplicates in the same scanning session,
-       * you're welcome ;)
-       */
-      continuousScanCallback?: Function;
-
-      /**
        * While scanning for a barcode show a button to flip to the other camera (front or back).
        * Default: false, so no flip button is shown.
        * Android only (on iOS the button is always shown).
@@ -55,13 +57,25 @@ declare module "nativescript-barcodescanner" {
        */
       orientation?: string;
     }
+  }
 
-    export function available(): Promise<boolean>;
-    export function hasCameraPermission(): Promise<boolean>;
-    export function requestCameraPermission(): Promise<boolean>;
-    export function scan(options: ScanOptions): Promise<any>;
+  /**
+   * The options object passed into the scan function.
+   */
+  export interface ScanOptions extends ScanOptions.IOS, ScanOptions.Android {
+  }
+
+  interface BarcodeScanner {
+    available(): Promise<boolean>;
+    hasCameraPermission(): Promise<boolean>;
+    requestCameraPermission(): Promise<boolean>;
+    scan(options: ScanOptions): Promise<any>;
     /**
      * Stop scanning, particularly useful when 'scan' was used with 'continuousScanCallback'
      */
-    export function stop(): Promise<any>;
+    stop(): Promise<any>;
+  }
 }
+
+declare var barcodeScanner: barcodeScanner.BarcodeScanner;
+export = barcodeScanner;
