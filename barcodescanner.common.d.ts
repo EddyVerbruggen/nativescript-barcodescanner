de0b21416e54e47a0e315cbe3f26e9babbd27ac4
Hristo Deshev <hristo@deshev.com>
2016-12-22 12:04:01 +0200

    @ng-tools/webpack compatibility
    The barcodescanner.ios reexport in the common module was causing
    the AoT-generated ngfactory module to include the ios module in
    the android bundle.

--- barcodescanner.common.d.ts
+++ barcodescanner.common.d.ts
@@ -18,3 +18,21 @@ export interface ScanOptions extends IOS, Android {
     IOS?: IOS;
     Android?: Android;
 }
+export declare class BarcodeScanner {
+    private _observer;
+    private _observerActive;
+    private _currentVolume;
+    private _scanner;
+    constructor();
+    private _hasCameraPermission;
+    private _hasDeniedCameraPermission;
+    private _addVolumeObserver;
+    private _removeVolumeObserver;
+    private _enableTorch;
+    private _disableTorch;
+    available(): Promise<boolean>;
+    hasCameraPermission(): Promise<boolean>;
+    requestCameraPermission(): Promise<boolean>;
+    stop(): Promise<any>;
+    scan(arg: ScanOptions): Promise<any>;
+}
