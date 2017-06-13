02a1a89250959ab85c71b0d1f651edc7be3056b0
EddyVerbruggen <eddyverbruggen@gmail.com>
2017-06-13 17:44:52 +0200

    setup issue #95

--- manual_typings/base.d.ts
+++
@@ -1,5 +0,0 @@
-/* Polyfill these types */
-declare type FormData = any;
-declare var FormData;
-declare type Blob = any;
-declare var Blob;
--- references.d.ts
+++ references.d.ts
@@ -1,5 +1,4 @@
-/// <reference path="./node_modules/tns-core-modules/lib.core.d.ts" /> Needed for autocompletion and compilation.
-/// <reference path="./node_modules/tns-core-modules/tns-core-modules.d.ts" /> Needed for autocompletion and compilation.
-/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
-/// <reference path="./node_modules/tns-platform-declarations/ios/ios.d.ts" />
+/// <reference path="./node_modules/tns-core-modules/tns-core-modules.d.ts" />
 
+/// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
+/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
