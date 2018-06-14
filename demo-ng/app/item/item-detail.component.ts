d61c4333e4162e99df8b24f434d059ecf6bedc01
EddyVerbruggen <eddyverbruggen@gmail.com>
2018-06-14 17:41:12 +0200

    Fixed demo-ng for iOS (embedded scanner didn't appear)

--- demo-ng/app/item/item-detail.component.ts
+++ demo-ng/app/item/item-detail.component.ts
@@ -1,62 +1,55 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute} from "@angular/router";
 
-import { Item } from "./item";
-import { ItemService } from "./item.service";
-
-import {isAndroid, isIOS} from "platform";
-import { BarcodeScanner, ScanOptions } from 'nativescript-barcodescanner';
+import {Item} from "./item";
+import {ItemService} from "./item.service";
+import {BarcodeScanner} from 'nativescript-barcodescanner';
 
 @Component({
-    selector: "ns-details",
-    moduleId: module.id,
-    templateUrl: "./item-detail.component.html",
+  selector: "ns-details",
+  moduleId: module.id,
+  templateUrl: "./item-detail.component.html",
 })
 export class ItemDetailComponent implements OnInit {
-    item: Item;
+  item: Item;
 
-    constructor(
-        private itemService: ItemService,
-        private route: ActivatedRoute,
-        private barcodeScanner: BarcodeScanner        
-    ) { }
+  constructor(
+      private itemService: ItemService,
+      private route: ActivatedRoute,
+      private barcodeScanner: BarcodeScanner) {
+  }
 
-    ngOnInit(): void {
-        const id = +this.route.snapshot.params["id"];
-        this.item = this.itemService.getItem(id);
-    }
+  ngOnInit(): void {
+    const id = +this.route.snapshot.params["id"];
+    this.item = this.itemService.getItem(id);
+  }
 
-    //Event handler for iOS embedded scanner
-    public onScanResult(evt) {
-        console.log(evt);
-        console.log(evt.object);
-    }
+  public onScanResult(evt) {
+    // console.log(evt.object);
+    console.log(`onScanResult: ${evt.text} (${evt.format})`);
+  }
 
-    //Event handler for button tap Android & iOS
-    public onScan($event): void {
-        let scan = () => {
-            this.barcodeScanner.scan({
-                formats:"QR_CODE, EAN_13",
-                beepOnScan:true,
-                reportDuplicates:true,
-                preferFrontCamera:false
-            }).then(result=> {
-                console.log(result);
-            }).catch(error => {
-                console.log(error);
-            }); 
-        }
+  public scanTapped(): void {
+    let scan = () => {
+      this.barcodeScanner.scan({
+        formats: "QR_CODE, EAN_13",
+        beepOnScan: true,
+        reportDuplicates: true,
+        preferFrontCamera: false
+        // continuousScanCallback: scanResult => {
+        //   console.log("result: " + JSON.stringify(scanResult));
+        //   this.barcodeScanner.stop();
+        // }
+      })
+          .then(result => console.log(JSON.stringify(result)))
+          .catch(error => console.log(error));
+    };
 
-        this.barcodeScanner.hasCameraPermission().then(
-            function(granted) {
-                scan();
-            }
-        ).catch(()=>{
-            this.barcodeScanner.requestCameraPermission().then(
-                function() {
-                  scan();
-                }
-            );
-        });       
-    }
+    this.barcodeScanner.hasCameraPermission()
+        .then(granted => granted ? scan() : console.log("Permission denied"))
+        .catch(() => {
+          this.barcodeScanner.requestCameraPermission()
+              .then(() => scan());
+        });
+  }
 }
