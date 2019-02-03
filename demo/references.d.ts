d60943528ca44feb11b2264d102a73a1adc98b15
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-02-03 12:34:43 +0100

    ci

--- demo/references.d.ts
+++ demo/references.d.ts
@@ -1,2 +1,4 @@
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
+
+/// <reference path="../src/platforms/ios/typings/BarcodeScannerFramework.d.ts" />
