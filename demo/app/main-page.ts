3d0592bcb89f1031873456e2fe3066e4fc6d9741
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-02-03 15:36:40 +0100

    ci

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,7 +1,6 @@
 import * as observable from "tns-core-modules/data/observable";
 import * as pages from "tns-core-modules/ui/page";
 import {HelloWorldModel} from "./main-view-model";
-import * as Toast from "nativescript-toast";
 
 // Event handler for Page "loaded" event attached in main-page.xml
 export function pageLoaded(args: observable.EventData) {
@@ -12,5 +11,4 @@ export function pageLoaded(args: observable.EventData) {
 
 export function onScanResult(scanResult: any) {
   console.log(`onScanResult: ${scanResult.text} (${scanResult.format})`);
-  Toast.makeText(`${scanResult.text} (${scanResult.format})`, "long").show();
 }
