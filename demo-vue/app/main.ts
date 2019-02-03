daaf828a5962370d6721978ca4499470b87ac2f7
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-02-03 12:10:14 +0100

    ci

--- demo-vue/app/main.ts
+++ demo-vue/app/main.ts
@@ -1,7 +1,7 @@
 import Vue from 'nativescript-vue';
 import App from './components/App.vue';
 
-(<any>Vue).registerElement('BarcodeScanner', () => require('nativescript-barcodescanner').BarcodeScannerView)
+(<any>Vue).registerElement('BarcodeScanner', () => require('nativescript-barcodescanner').BarcodeScannerView);
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production');
